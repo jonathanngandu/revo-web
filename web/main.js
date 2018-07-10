44184ca206eef1ae14b75b718b1625705d1c96d9
Jonathan.Ngandu <jonathan.ngandu@sky.uk>
2018-07-10 21:32:16 +0100

    homepage designed - nav, header, background and video

---
+++ web/main.js
@@ -0,0 +1,9 @@
+import 'js/bootstrap.min';
+import 'js/jquery.isotope';
+import 'js/jquery.min';
+import 'js/jquery.slicknav';
+import 'js/jquery.sticky';
+import 'js/jquery.visible';
+import 'js/modernizr.custom';
+import 'js/scripts';
+import 'js/slimbox2';
